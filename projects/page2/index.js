abee6d67e336cbbb2ff16e72b1af7cf990f2cb50
alpha <827130441@qq.com>
2019-08-19 12:45:53 +0800

    feat: hot-module-replacement

--- projects/page2/index.js
+++ projects/page2/index.js
@@ -1,4 +1,8 @@
 console.log("page2");
+console.log("page2-1");
+console.log("page2-2");
+
+import "./page2.css";
 
 import { demo1 } from "common";
 demo1();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const fs = require("fs");
+const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 function multipleEntry() {
   const projectsBaseDir = path.resolve(__dirname, "projects");
@@ -19,10 +21,31 @@ function multipleEntry() {
       entryObj["others"].push(projectDir);
     }
   });
-  console.log(`TCL: multipleEntry -> entryObj`, entryObj);
   return entryObj;
 }
-// multipleEntry();
+
+function makePlugins() {
+  const projectsBaseDir = path.resolve(__dirname, "projects");
+  const projects = fs.readdirSync(projectsBaseDir);
+  const plugins = [];
+  projects.forEach(project => {
+    const projectDir = path.resolve(projectsBaseDir, project);
+    if (
+      fs.statSync(projectDir).isDirectory() &&
+      fs.readdirSync(projectDir).indexOf("index.js") > -1
+    ) {
+      // projects下的文件夹有index.js文件
+      plugins.push(
+        new HtmlWebpackPlugin({
+          title: project,
+          filename: `${project}/index.html`,
+          chunks: [project]
+        })
+      );
+    }
+  });
+  return plugins;
+}
 
 module.exports = {
   mode: "development",
@@ -36,18 +59,38 @@ module.exports = {
   },
   entry: multipleEntry(),
   output: {
+    publicPath: "/",
     path: path.resolve(__dirname, "dist"),
     filename: "[name]/bundle.[hash:5].js"
   },
   module: {
     noParse: /jquery|lodash/,
-    rules:[]
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    ...makePlugins()
+  ],
   devServer: {
+    port: 9998,
     contentBase: path.resolve(__dirname, "dist"),
     hot: true,
+    
     // lazy: true,
-    open: true,
+    // open: true
   }
 };
